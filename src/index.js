960df305aaf2e080b3e43dfe2fc2eb70ec2f5b28
Remi Leguerneve <remi.le_guerneve@etudiant.univ-lr.fr>
2024-10-21 11:13:04 +0200

    fix ci

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './root.sass';
-import GridCardTrack from './components/GridCardTrack';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
